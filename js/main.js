5ac6a994e39aa91db5f755dc7ee0975a1b465801
minhluan306 <minhluan306288@gmail.com>
2023-03-22 21:44:38 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// const inputNumber = (a,b,callBack) => {
+//    if(!a || !b) return console.log('Vui lòng nhập đủ số a và b!')
+//    return callBack(a,b)
+// }
+
+
+// const a = '124567'.split('')
+// const save = []
+// a.forEach((v,i,a) => {
+//    console.log('i',i);
+//    console.log('a',a);  
+//    save.unshift(v)
+// },[])
+// console.log('save',save);
+// const kq = save.join('')
